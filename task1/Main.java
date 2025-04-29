03d3c82b3bd0c6ea6a908d8728a66fc5ccf0cd32
Kazybek_S <kazybek.dev2025@gmail.com>
2025-04-29 17:04:35 +0600

    added sort by breed

--- task1/Main.java
+++ task1/Main.java
@@ -1,5 +1,7 @@
 package kg.attractor.java.task1;
 
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,10 +9,14 @@ public class Main {
         var cats = Cat.makeCats(10);
         Printer.print(cats);
 
-        // А сюда добавьте код, который будет сортировать коллекцию котов
-        // используйте лямбда-выражения и ссылки на методы
-        // cats.sort(?);
-        // Printer.print(cats);
+        // Сортировка по породе (используем ссылку на метод)
+        cats.sort(Comparator.comparing(Cat::getBreed));
+        System.out.println("Сортировка по породе:");
+        Printer.print(cats);
+
+
+
+
     }
 
 }
